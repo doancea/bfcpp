f0a90f9812a958843a53bbd093b3b1cb173ef4a0
doancea <dano4432@gmail.com>
2014-04-15 00:12:15 -0400

    Built out the tests for Box class a bit more.  Writing tests after the fact because I needed working code to validate the working tests

--- shapes/tests/testbox.cpp
+++ shapes/tests/testbox.cpp
@@ -1,10 +1,34 @@
 #include "../src/box.cpp"
+#include <string>
 #include <gtest/gtest.h>
 
 TEST(GetHeightTest, GetsHeight) {
 	int height = 10;
 	Box b(height, 5, '*');
 	ASSERT_EQ(height, b.getHeight());
+}
+
+TEST(FirstRowTest, TestsFirstRowOutput) {
+	int width = 5;
+	char c = '*';
+	Box b(10, width, c);
+	EXPECT_EQ(std::string(width, c), b.getRow(1)); 
+}
+
+TEST(LastRowTest, TestsLastRowOutput) {
+	int height = 10, width = 5;
+	char c = '*';
+	Box b(height, width, c);
+	EXPECT_EQ(std::string(width, c), b.getRow(height));
+}
+
+TEST(BoxEmptyTest, TestsMidRowsContainBlanks) {
+	int height = 10, width = 5, i = 2;
+	char c = '*';
+	Box b(height, width, c);
+	for(i; i < height; i++) {
+		EXPECT_NE(std::string(width, c), b.getRow(i));
+	}
 }
 
 int main(int argc, char **argv) {
